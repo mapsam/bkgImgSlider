4048ec07d52981a8e94d33f0a48ae62f32c1e8a7
Sam Matthews <matthews.sam@gmail.com>
2013-03-19 17:57:40 -0700

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,24 +1,47 @@
 $(window).load(function(){
-	//global variables
-	var images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg']; //Your images in an array. This can be as large/small as you like
+	//global variables	
+	//Your images in an array. This can be as large/small as you like
+	var images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg']; 
 	var i = 0; //do not change
 	var q = 1; //do not change
-	var transitionLength = 6000 //Total time image is viewable, inlcluding the transition
+	
+	//Total time image is viewable, inlcluding the transition
+	var transitionLength = 6000 
+	
+	//and so it begins (endless loop)
 	function changeBackground() {
-		$('#front').css('background-image', function() { //changes front div image
-			if (i >= images.length) { //allows loop to restart once final image loads
+
+		//changes front div image
+		$('#front').css('background-image', function() { 
+
+			//allows loop to restart once final image loads
+			if (i >= images.length) { 
 				i = 0;
 			}
-			return 'url(images/' + images[i++] + ')'; //returns an image string to the css background-image property
+
+			//returns an image string to the css background-image property
+			return 'url(images/' + images[i++] + ')'; 
 		});
-		$('#main').css('background-image', function() { //changes back image to the following image so fade 
+		
+		//changes back image to the following image so fade 
+		$('#main').css('background-image', function() { 
+			
+			//allows loop to restart once final image loads
 			if (q >= images.length) {
 				q = 0;
 			}
+
+			//returns an image string to the css background-image property
 			return 'url(images/' + images[q++] + ')';
 		});
-		$('#front').delay(transitionLength/4).fadeOut(4000).show(); //delayed fade out after images are loaded, then shows the div again
-		time = setTimeout(changeBackground, transitionLength); //runs function and loops FOREVER
+
+		//delayed fade out after images are loaded, then shows the div again
+		$('#front').delay(transitionLength/4).fadeOut(4000).show(); 
+
+		//runs function and loops FOREVER
+		time = setTimeout(changeBackground, transitionLength); 
 	}
-	changeBackground(); //funs the function for the first time
+
+	//funs the function for the first time
+	changeBackground(); 
 });
